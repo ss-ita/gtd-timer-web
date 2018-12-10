4e3cab613db3ff106b70a555d9b386f411849c77
Oleksandr-Lytvyn <oleksandrlytvyn1223@gmail.com>
2018-12-10 14:54:20 +0200

    created folders for sign in and sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlarmComponent } from './components/alarm/alarm.component';
+import { AlertComponent } from './components/alert/alert.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlarmComponent,
+    AlertComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule
