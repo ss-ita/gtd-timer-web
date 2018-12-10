7b1ccf75d5b3fbd89c611a72ead9d1953d1e5eee
Oleksandr-Lytvyn <oleksandrlytvyn1223@gmail.com>
2018-12-10 15:24:39 +0200

    import changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AlarmComponent } from './components/alarm/alarm.component';
-import { AlertComponent } from './components/alert/alert.component';
-import { SignupComponent } from './signup/signup.component';
-import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
