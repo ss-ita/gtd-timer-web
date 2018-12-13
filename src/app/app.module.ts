20421a17b2178e41711c27a24932bb65e0bc9f77
Oleksandr-Lytvyn <oleksandrlytvyn1223@gmail.com>
2018-12-13 11:32:12 +0200

    Closes #2 - Create a Sign in page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { SigninComponent } from './auth/signin/signin.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
+import { CustomMaterialModule } from '../app/material.module'
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './auth/signup/signup.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    CustomMaterialModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // no need to place any providers due to the `providedIn` flag...
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Router} from '@angular/router';
+import {FormControl, Validators} from '@angular/forms';
+import {SignService} from '../../sign.service'
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
+
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+
+  getErrorMessageEmail() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+  getErrorMessagePassword() {
+    return this.password.hasError('required') ? 'You must enter your password' :
+        this.password.hasError('minlength') ? 'You must enter 6 elements min' :
+            '';
+  }
+
+  constructor(private router: Router ,private Sign:SignService) { }
 
   ngOnInit() {
   }
 
+  signin(event) 
+  {
+    const target=event.target
+    const email=target.querySelector('#email').value
+    const password=target.querySelector('#password').value
+    event.preventDefault()
+    this.Sign.getUserDetails(email,password)
+    console.log(email,password)
+
+  }
 }
