1523d9395a2e68395cc5d0c6eb8f416bc58a0549
Oleksandr-Lytvyn <oleksandrlytvyn1223@gmail.com>
2019-01-23 16:36:02 +0200

    before changes

--- src/app/services/archive.service.ts
+++ src/app/services/archive.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { ConfigService } from './config.service';
 import { Observable } from 'rxjs';
 import { TaskJson } from '../models/taskjson.model';
